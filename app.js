c4bb897c6169e18653e7f07f5a385061517071bc
Cobra Bubbles <mjoeng@hotmail.no>
2021-02-03 16:11:30 +0100

    node og express initialisering

---
+++ app.js
@@ -0,0 +1,27 @@
+// Imports 
+const { text } = require('express');
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
+
+
+// Static files 
+app.use(express.static('public')); 
+app.use('/css', express.static(__dirname + 'public/css')); 
+app.use('/js', express.static(__dirname + 'public/js')); 
+app.use('/img', express.static(__dirname + 'public/img')); 
+
+app.set('views', './views'); 
+app.set('view engine', 'ejs'); 
+
+app.get('', (req, res) => {
+    res.render('index', {text: 'This is ejs'});
+});
+
+app.get('', (req, res) => {
+    res.render('other', {text: 'other pages'}); 
+});
+
+
+// Listen on port 3000 
+app.listen(port, () => console.info(`Listening on port ${port}`)); 
