1a3d38918779939b03852d135032e888a3c2c2e3
Cobra Bubbles <mjoeng@hotmail.no>
2021-02-04 18:28:44 +0100

    Loking rundt

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.get('', (req, res) => {
     res.render('index', {text: 'This is ejs'});
 });
-
+ 
 app.get('', (req, res) => {
     res.render('other', {text: 'other pages'}); 
 });
