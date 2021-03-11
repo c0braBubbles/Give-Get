7d40629e931ea3a5e0d7b5c4bacbbfd29fcd2b77
Cobra Bubbles <mjoeng@hotmail.no>
2021-03-11 20:58:35 +0100

    Sidene er nå synlige for serveren

--- app.js
+++ app.js
@@ -6,33 +6,51 @@ const port = 3000;
 
 
 // Static files 
+// Initierer bruk av mappene
 app.use(express.static('public')); 
 app.use('/css', express.static(__dirname + 'public/css')); 
 app.use('/js', express.static(__dirname + 'public/js')); 
 app.use('/img', express.static(__dirname + 'public/img')); 
 
+
+// Setter enginge for views. Hvordan lese filene
 app.set('views', './views'); 
 app.set('view engine', 'ejs'); 
 
+
+// Requester alle sidene
+// Denne er index/landing-siden
 app.get('', (req, res) => {
     res.render('', {text: 'This is ejs'});
+});
+
+// DETTE SKAL BLI EN PARTIAL ETTERHVERT
+app.get('/annonser', (req, res) => {
+    res.render('annonser');
+}); 
+
+// PROBLEMER MED DENNE. SPØR JACOB ELLER CHRIS
+// DETTE SKAL VÆRE BLI EN PARTIAL ETTERHVERT
+app.get('/editprofile', (req, res) => {
+    res.render('editprofile');
 });
 
 app.get('/innstillinger', (req, res) => {
     res.render('innstillinger'); 
 });
- 
-app.get('/other', (req, res) => {
-    res.render('other', {text: 'other pages'}); 
+
+app.get('/login', (req, res) => {
+    res.render('login'); 
 });
 
-app.get('/header', (req, res) => {
-    res.render('header'); 
-}); 
+app.get('/profile', (req, res) => {
+    res.render('profile');
+});
 
-app.get('/testest', (req, res) => {
-    res.render('testest'); 
-}); 
+// DENNE KAN VÆRE MULIG VI DROPPER
+app.get('/registerer', (req, res) => {
+    res.render('registerer');
+});
 
 
 // Listen on port 3000 
