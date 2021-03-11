73617e3ecfd6fd0af3706dba1b85f0ee0cc8a911
🎃 Christoffer 🎃 <strand.arnesen@gmail.com>
2021-03-11 15:18:37 +0100

    fikset path til css filer, lagt inn JS for å finne lokasjon i innstillinger siden

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs'); 
 
 app.get('', (req, res) => {
-    res.render('index', {text: 'This is ejs'});
+    res.render('innstillinger', {text: 'This is ejs'});
 });
  
 app.get('/other', (req, res) => {
