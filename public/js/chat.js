c65ddc44b9dee7318d5713167c3830c00def4c88
🌝Jacob🌚 <Jacobkristensen@hotmail.no>
2021-05-24 18:21:15 +0200

    chat script

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,7 +2,7 @@
  * JavaScript fil for all funksjonalitet som har med chat-delen 
  * av applikasjonen å gjøre
  * 
- * Script skrevet av Mats Jørgen Engesund, utenom der det eventuelt er markert i koden
+ * Skrevet av Jacob Kristensen og Mats Engesund
  */
 
 
@@ -18,146 +18,148 @@ var chatWindow = document.getElementById("chatVindu2");         // Hvor meldings
 var chatListLeft = document.getElementById("chatListeVenstre"); // Listen med alle samtaler
 var chatListTop = document.getElementById("chatListeTopp");     // Når vinduet blir minimert skal samtaler dukke opp i denne div-en
 
-var maxChat = 0;        // Max antall samtaler totalt
-var currentChat = 0;    // Samtalen du for øyeblikket er på.
+
 
 
 
 /**
- * Tar inn brukernavnet fra annonsen som parameter
- * og re-directer brukeren til en privat chat hvor 
- * han/hun kan snakke sammen
+ * Tar inn brukernavn, annonseID og annonsetittel fra en annonse
+ * som inn-parameter. Funksjonen kalles på når en trykker på "chat-bobla" til en annonse
+ * på kartet. Funksjonen har ansvar for å opprette "samtale" i realtime-databasen, men sjekker først:
+ *   1. Har brukeren en allerede-eksisterende samtale til denne annonsen?
+ *   2. Er det eieren av annonsen som prøver å opprette en samtale med seg selv?
+ * Dersom ingen av de to stemmer -> Opprett en samtale i realtime-databasen
  * 
  * @param {navnet til den man vil prate med} name 
  * @param {annonsetittel} addTittle 
+ * @param {annonseID} enID
  */
-
-function startChat(name, addTittle) {
-    // henter siste chat lagt til og øker med 1
-    chat_ref.on("child_added", function (snapshot) {
-        var message = snapshot.val();
-        maxChat = message.chatID;
-        maxChat++;
+function lagSamtale(name, addTitle, enID) {
+    var samtaleID = Date.now();
+    var finnesSamtale = false;
+    
+    firebase.database().ref("samtale").once("value", (snapshot) => {
+        var data = snapshot.val();
+        if (snapshot.exists()) {
+            for (let i in data) {
+                if (data[i].annonseID == enID && data[i].annonseInteressert == username && finnesSamtale == false) {
+                    alert("Du har allerede en samtale knyttet til denne annonsen");
+                    finnesSamtale = true;
+                } else if (name === username) {
+                    alert("Du er eier av annonsen");
+                    finnesSamtale = true;
+                }
+            }
+        }
+        if (finnesSamtale == false) {
+            firebase.database().ref('samtale').child(samtaleID).set({
+                annonseEier : name,
+                annonseInteressert : username,
+                annonseTittel : addTitle,
+                annonseID : enID
+            }).then(() => {
+                active.style.display = "none";
+                toggles[2].style.display = "block";
+            });
+        }
     });
-
-    active.style.display = "none";
-    toggles[2].style.display = "block";
-
-    // Sender informasjon om samtale til DB
-    chat_ref.push({
-        "chatID": maxChat,
-        "sender": username,
-        "mottaker": name,
-        "tittel": addTittle
-    });
+    
 }
-
-
-
-/**
- * Henter samtaler som er startet og
- * legger dem til i venstremenyen 
- * og toppmenyen
+/* 
+ * Koden under har som ansvar å legge til innhold i html-elementer på chat-siden
+ * når en samtale blir opprettet i realtime-databasen. Innhold skal bli gitt til
+ * riktig brukere, altså brukeren som starter/oppretter samtalen, og eier av den aktuelle annonsen
+ * 
  */
-
-chat_ref.on("child_added", function (snapshot) {
+firebase.database().ref('samtale').on("child_added", function (snapshot) {
     var message = snapshot.val();
 
-    if (message.sender === username) {
-        chatListLeft.innerHTML +=   `<li class="another-chat" onclick="openChat(` + message.chatID + `)">` +
+    if (message.annonseInteressert === username) {  //Dersom du er den som tar kontakt
+        chatListLeft.innerHTML +=  `<li class="another-chat" onclick="openChat(` + snapshot.key + `)">` + //NB! Husk å lage egen metode, istedet for openChat
                                         `<br>` +
-                                        `<h5>` + message.mottaker + `</h5>` +
-                                        `<p>` + message.tittel + `</p>` + 
+                                        `<h5>` + message.annonseEier + `</h5>` +
+                                        `<p>` + message.annonseTittel + `</p>` + 
                                         `<br>` +
-                                    `</li>`;
-
-        // Til dropdown menyen når vinduet blir mindre (responsivt)
-        chatListTop.innerHTML +=    `<li class="another-chat" onclick="openChat(`+ message.chatID + `)">  
+                                   `</li>`;
+        chatListTop.innerHTML +=    `<li class="another-chat" onclick="openChat(`+ snapshot.key + `)">  
                                         <div>
-                                            <h5>` + message.mottaker + `</h5>` + 
-                                            `<p>` + message.tittel + `</p>` + 
+                                            <h5>` + message.annonseEier + `</h5>` + 
+                                            `<p>` + message.annonseTittel + `</p>` + 
                                         `</div>
                                     </li>`
-    } else if (message.mottaker === username) {
-        chatListLeft.innerHTML +=   `<li class="another-chat" onclick="openChat(` + message.chatID + `)">` +
+    } else if (message.annonseEier === username) {
+        chatListLeft.innerHTML +=  `<li class="another-chat" onclick="openChat(` + snapshot.key + `)">` + //NB! Husk å lage egen metode, istedet for openChat
                                         `<br>` +
-                                        `<h5>` + message.sender + `</h5>` +
-                                        `<p>` + message.tittel + `</p>` + 
+                                        `<h5>` + message.annonseInteressert + `</h5>` +
+                                        `<p>` + message.annonseTittel + `</p>` + 
                                         `<br>` +
-                                    `</li>`;
-
-        // Til dropdown menyen når vinduet blir mindre (responsivt)
-        chatListTop.innerHTML +=    `<li class="another-chat" onclick="openChat(`+ message.chatID + `)">  
+                                   `</li>`;
+        chatListTop.innerHTML +=    `<li class="another-chat" onclick="openChat(`+ snapshot.key + `)">  
                                         <div>
-                                            <h5>` + message.sender + `</h5>` +
-                                            `<p>` + message.tittel + `</p>` + 
+                                            <h5>` + message.annonseInteressert + `</h5>` + 
+                                            `<p>` + message.annonseTittel + `</p>` + 
                                         `</div>
                                     </li>`
     }
 });
 
-
-
-/**
- * Sende melding fra tekstfelt til DB
- * @param {*} evt 
+/*
+ * Metoden aktiveres når en bruker trykker på en samtale (boks på venstre side av chat-siden/dropdown meny)
+ * Den har ansvar for å hente tidligere meldinger fra den allerede-eksisterende samtalen slik at bruker kan se
+ * hva de to partene har skrevet til hverandre tidligere. 
  */
+var aktivSamtaleID;
+function openChat(samtaleNr) {
+    chatWindow.innerHTML = ``; // Fjerner alle meldinger, og så skal meldingene hentes på ny
+    aktivSamtaleID = samtaleNr;
 
-function sendMld() {
-form.onsubmit = function (evt) {
-    //Logging av chat - Christoffer
-    analytics.logEvent('melding_sendt', {
-        melding: msg_inp.value,
-        sender: username,
+    firebase.database().ref("melding/").once('value', (snapshot) => {
+        var data = snapshot.val();
+        for (let i in data) {
+            if (data[i].samtaleNr === samtaleNr) { 
+                if (username === data[i].sender) {
+                    chatWindow.innerHTML += `<div id='bobler' class='msg-line'><p class='sender-bubble'>${data[i].beskjeden}</p></div>`;
+                } else {
+                    chatWindow.innerHTML += `<div id='bobler' class="msg-line"><p class="receiver-bubble">${data[i].beskjeden}</p></div>`;
+                }
+            }
+        }
     })
+}
+
+/*
+ * Koden under aktiveres når en "submitter" noe i formen, altså når bruker trykker på send-knappen, eller
+ * trykker enter etter en har skrevet en melding i input-feltet på chat-siden.
+ * Koden sender opp meldingen til realtime-databasen, og setter input-feltet til blank 
+ */
+//sendMelding
+form.onsubmit = function (evt) {
     evt.preventDefault();
-    chat_msg.push({
-        "chatID": currentChat,
-        "meldingen": msg_inp.value,
-        "brukernavnet": username
+    var testmeldingID = Date.now();
+    firebase.database().ref("melding").child(testmeldingID).set({
+        samtaleNr : aktivSamtaleID,
+        beskjeden : msg_inp.value,
+        sender : username
     });
     msg_inp.value = "";
-    openChat(currentChat);
-}
-console.log("kjører");
 }
 
-
-
-/**
- * Åpner gamle meldinger fra samtaler 
- * når du trykker på en samtale du vil 
- * åpne i venstre/top-menyen
- * @param {*} number 
+/*
+ * Koden under aktiveres når en ny melding blir lagt til i realtime-databasen, og henter meldingen som ble sendt
+ * Meldingen blir bare vist frem dersom den ble sendt i aktive samtalen, og skiller mellom om det var du eller brukeren
+ * du snakker med som sendte meldingen. 
  */
-
-function openChat(number) {
-    // chatWindow.innerHTML = ``;
-    currentChat = number;
-
-    chat_msg.on("child_added", function (snapshot) {
-        var message = snapshot.val();
-        if (message.chatID === currentChat) {
-            if (username == message.brukernavnet) {
-                chatWindow.innerHTML += `<div id='bobler' class='msg-line'><p class='sender-bubble'>${message.meldingen}</p></div>`;
-            } else {
-                chatWindow.innerHTML += `<div id='bobler' class="msg-line"><p class="receiver-bubble">${message.meldingen}</p></div>`;
-            }
+firebase.database().ref("melding").on("child_added", function (snapshot) {
+    var data = snapshot.val();
+    //Sjekker om meldingen ble sendt til nåværende samtale
+    if (data.samtaleNr == aktivSamtaleID) {
+        // sant - er sender deg, eller er sender en annen. Er det deg? -> sett riktig boble osv
+        if (data.sender == username) {
+            chatWindow.innerHTML += `<div id='bobler' class='msg-line'><p class='sender-bubble'>${data.beskjeden}</p></div>`;
+        } else {
+            chatWindow.innerHTML += `<div id='bobler' class="msg-line"><p class="receiver-bubble">${data.beskjeden}</p></div>`;
         }
-    });
-}
+    }
+    
+});
 
-
-
-function getData() {
-    chat_msg.on("child_added", function (snapshot) {
-        var message = snapshot.val();
-        if (message.chatID === currentChat) {
-            if (username == message.brukernavnet) {
-                chatWindow.innerHTML += `<div id='bobler' class='msg-line'><p class='sender-bubble'>${message.meldingen}</p></div>`;
-            } else {
-                chatWindow.innerHTML += `<div id='bobler' class="msg-line"><p class="receiver-bubble">${message.meldingen}</p></div>`;
-            }
-        }
-    });
-}
