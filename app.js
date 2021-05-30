6a7215e24d4fcaceff08cccdfb88cdd22edfaf51
FrostyJ <mjoeng@hotmail.no>
2021-05-30 15:05:46 +0200

    Endret app.js til å være tilpasset serveren

--- app.js
+++ app.js
@@ -4,9 +4,46 @@
 */
 
 
+// Imports
+var path = require('path'); 
+var express = require('express'); 
+
+
+/* Setter engine for prosjektet. Hvordan bruker kan se sidene */
+var app = express();
+app.set('view engine', 'ejs'); 
+app.set('views', path.resolve(__dirname, 'views')); 
+
+
+/* Henting av sidene når brukeren forespør en av de tre hovedsidene */
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/main', (req, res) => {
+    res.render('main'); 
+});
+
+app.get('/registerer', (req, res) => {
+    res.render('registerer');
+});
+
+
+// Gjør mappen public synlig, så app-en kan ta bruk av:
+app.use(express.static(path.resolve(__dirname, 'public')));
+//Avgjør hvilken port programmet skal "lytte" på:
+app.listen(process.env.PORT || 1337);
+
+
+
+
+
+
+
+
 
 // Imports 
-const { text } = require('express');
+/*const { text } = require('express');
 const express = require('express'); 
 const app = express(); 
 const port = 3000; 
@@ -43,4 +80,4 @@ app.get('/registerer', (req, res) => {
 
 
 // Listen on port 3000 
-app.listen(port, () => console.info(`Listening on port ${port}`)); 
+app.listen(port, () => console.info(`Listening on port ${port}`)); */
